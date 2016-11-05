28c899dc69f237a481fcd13e8c76c1b53cbc17e0
biccon <admin@idx.kr>
2016-11-05 16:37:40 +0900

    global variables

--- main.c
+++ main.c
@@ -7,18 +7,25 @@
 #include "clean.h"
 #include "state.h"
 
-
-int main(){
-	btn_ctx btn_on;
-	btn_ctx btn_off;
-	btn_ctx btn_extract;
-	btn_ctx btn_concentration;
-	btn_ctx btn_supply;
-	btn_ctx btn_reservation_clean;
-	btn_ctx btn_reservation_extract;
-	btn_ctx btn_reservation_cancel;
-	btn_ctx btn_clean;
+btn_ctx btn_on;
+btn_ctx btn_off;
+btn_ctx btn_extract;
+btn_ctx btn_concentration;
+btn_ctx btn_supply;
+btn_ctx btn_reservation_clean;
+btn_ctx btn_reservation_extract;
+btn_ctx btn_reservation_cancel;
+btn_ctx btn_clean;
+sensor_ctx sensor_hot_weight;
+sensor_ctx sensor_cold_weight;
+sensor_ctx sensor_cup_existence;
+sensor_ctx sensor_coffee_bean_weight;
+sensor_ctx sensor_coffee_powder_weight;
+sensor_ctx sensor_hot_temperature;
+sensor_ctx sensor_cold_temperature;
 	
+int main(){
+
 	btn_init(&btn_on, 'o');
 	btn_init(&btn_off, 'f');
 	btn_init(&btn_extract, 'e');
@@ -28,14 +35,6 @@ int main(){
 	btn_init(&btn_reservation_extract, 't');
 	btn_init(&btn_reservation_cancel, 'a');
 	btn_init(&btn_clean, 'n');
-	
-	sensor_ctx sensor_hot_weight;
-	sensor_ctx sensor_cold_weight;
-	sensor_ctx sensor_cup_existence;
-	sensor_ctx sensor_coffee_bean_weight;
-	sensor_ctx sensor_coffee_powder_weight;
-	sensor_ctx sensor_hot_temperature;
-	sensor_ctx sensor_cold_temperature;
 	
 	sensor_init(&sensor_hot_weight, “hot_weight.txt”, 0, 500);
 	sensor_init(&sensor_cold_weight, “cold_weight.txt”, 0, 500);
